6e15b87bee76b943f6a54f43899442c630169f6c
James MacIvor <james@macivortech.com>
2022-03-13 10:59:57 -0400

    added Startup.cs, stubbed out basic wizard actions

---
+++ wizard-potion-app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(wizard_potion_app.Startup))]
+namespace wizard_potion_app
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //builder.Services.AddSingleton<NothingClient>();
+        }
+    }
+}
--- wizard-potion-app/WizardActions.cs
+++ wizard-potion-app/WizardActions.cs
@@ -10,10 +10,30 @@ using Newtonsoft.Json;
 
 namespace wizard_potion_app
 {
-    public static class Function1
+    public static class WizardActions
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
+        [FunctionName("CreateWizard")]
+        public static async Task<IActionResult> CreateWizard(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("GetWizard")]
+        public static async Task<IActionResult> GetWizard(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
